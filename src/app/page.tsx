9094e398c43830cdb9dce2a74b0018f5caf9fda7
ramyus889 <ramyus889.com>
2024-04-23 11:31:24 +0300

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 export default function Home() {
   return (
     <div className="flex place-content-center font-['Inter']">
-      <div className="w-[400px] mt-20 px-3">
+      <div className="w-[400px] mt-20 ps-5 pe-10">
         <Head />
         <History />
         <Footer />
